3b73cea761adc42790678a9092753e8e82f29a86
ignaciosm <ignaciosm@gmail.com>
2019-12-19 16:11:45 -0500

    setup

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const Users = require("./users/users-model");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up", dbenv: process.env.DB_ENV });
+});
+
+server.get("/users", (req, res) => {
+  Users.getAll()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/users", (req, res) => {
+  Users.insert(req.body)
+    .then(([id]) => {
+      res.status(201).json(id);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = server;
